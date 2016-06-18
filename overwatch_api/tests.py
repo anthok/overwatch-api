b84194d323fb7c44dea6c6314ae1e0b7641a6c0c
anthok <anthok>
2016-06-18 07:21:04 -0700

    Adding print messages to tests

--- overwatch_api/tests.py
+++ overwatch_api/tests.py
@@ -3,8 +3,19 @@ from overwatch_api import *
 ow = OverwatchAPI('key')
 
 ow.get_patch_notes()
+print ('Patch notes passed')
+
 ow.get_user_achievements(PC,AMERICAS,'elyK-1940')
+print ('User achievements passed')
+
 ow.get_stats_all_heroes(PC,AMERICAS,'elyK-1940')
+print ('Stats all heroes passed')
+
 ow.get_stats_one_hero(PC,AMERICAS,'elyK-1940',heroes['MERCY'])
+print ('Stats one hero passed')
+
 ow.get_stats(PC,AMERICAS,'elyK-1940')
+print ('Player stats passed')
+
 ow.get_profile(PC,AMERICAS,'elyK-1940')
+print ('User profile passed')
