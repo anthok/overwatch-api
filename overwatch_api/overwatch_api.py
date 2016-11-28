e7f46d2cf0dcfc6adebff069b5f9c627c3fee2b4
Bill Pond <william.e.pond@gmail.com>
2016-11-28 13:53:35 -0500

    Adding Sombra to Heros List

--- overwatch_api/overwatch_api.py
+++ overwatch_api/overwatch_api.py
@@ -33,6 +33,7 @@ heroes = {
     'REINHARDT': 'Reinhardt',
     'ROADHOG': 'Roadhog',
     'SOLDIER_76': 'Soldier76',
+    'SOMBRA': "Sombra",
     'SYMMETRA': 'Symmetra',
     'TORBJOERN': 'Torbjoern',
     'TRACER': 'Tracer',
