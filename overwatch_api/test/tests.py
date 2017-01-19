f2f2a3e54fe610253570d6ad720d800008307865
Emad Mokhtar <emad.m.habib@gmail.com>
2017-01-19 14:15:59 +0300

    Fix tests

--- overwatch_api/test/tests.py
+++ overwatch_api/test/tests.py
@@ -8,10 +8,12 @@ class TestPatchNotes(TestCase):
     def setUp(self):
         self.ow = OverwatchAPI('key')
 
+    @vcr.use_cassette('fixtures/vcr_cassettes/get_patch_notes.yaml')
     def test_get_patch_notes(self):
         result = self.ow.get_patch_notes()
         self.assertIsInstance(result, dict)
 
+    @vcr.use_cassette('fixtures/vcr_cassettes/get_patch_notes.yaml')
     def test_get_patch_notes_keys(self):
         result = self.ow.get_patch_notes()
         self.assertEqual(set(result.keys()), set(['patchNotes', 'pagination']))
@@ -32,14 +34,14 @@ class TestUserMethods(TestCase):
         result = self.ow.get_profile(PC, AMERICAS, 'elyK-1940')
         self.assertIn('data', result.keys())
 
-    @vcr.use_cassette('fixtures/vcr_cassettes/get_user_achievements.yaml')
+    @vcr.use_cassette('fixtures/vcr_cassettes/get_achievements.yaml')
     def test_get_user_achievements(self):
-        result = self.ow.get_user_achievements(PC, AMERICAS, 'elyK-1940')
+        result = self.ow.get_achievements(PC, AMERICAS, 'elyK-1940')
         self.assertIsInstance(result, dict)
 
-    @vcr.use_cassette('fixtures/vcr_cassettes/get_user_achievements.yaml')
+    @vcr.use_cassette('fixtures/vcr_cassettes/get_achievements.yaml')
     def test_get_user_achievements_keys(self):
-        result = self.ow.get_user_achievements(PC, AMERICAS, 'elyK-1940')
+        result = self.ow.get_achievements(PC, AMERICAS, 'elyK-1940')
         expected_keys = set(['totalNumberOfAchievements',
                              'numberOfAchievementsCompleted',
                              'finishedAchievements', 'achievements'])
@@ -97,6 +99,7 @@ class TestUserMethods(TestCase):
                              'ObjectiveTime',
                              'TimePlayed',
                              'MeleeFinalBlow-MostinGame',
+                             'ReconAssists-Average',
                              'DefensiveAssists',
                              'DefensiveAssists-Average',
                              'OffensiveAssists',
@@ -105,23 +108,24 @@ class TestUserMethods(TestCase):
 
     @vcr.use_cassette('fixtures/vcr_cassettes/get_stats_one_hero.yaml')
     def test_get_stats_one_hero(self):
-        result = self.ow.get_stats_one_hero(PC, AMERICAS, 'elyK-1940', QUICK,
-                                            heroes['MERCY'])
+        result = self.ow.get_stats_selected_heroes(PC, AMERICAS, 'elyK-1940', QUICK,
+                                                   [heroes['MERCY']])
         self.assertIsInstance(result, dict)
 
     @vcr.use_cassette('fixtures/vcr_cassettes/get_stats_one_hero.yaml')
     def test_get_stats_one_hero_keys(self):
-        result = self.ow.get_stats_one_hero(PC, AMERICAS, 'elyK-1940', QUICK,
-                                            heroes['MERCY'])
+        result = self.ow.get_stats_selected_heroes(PC, AMERICAS, 'elyK-1940',
+                                                   QUICK, [heroes['MERCY']])
+        print(result)
         self.assertIn('Mercy', result.keys())
         self.assertIn('PlayersResurrected', result['Mercy'].keys())
 
     @vcr.use_cassette('fixtures/vcr_cassettes/get_stats.yaml')
     def test_get_stats(self):
-        result = self.ow.get_stats(PC, AMERICAS, 'elyK-1940', QUICK)
+        result = self.ow.get_stats_heroes_used(PC, AMERICAS, 'elyK-1940', QUICK)
         self.assertIsInstance(result, list)
 
     @vcr.use_cassette('fixtures/vcr_cassettes/get_stats.yaml')
     def test_get_stats_amount(self):
-        result = self.ow.get_stats(PC, AMERICAS, 'elyK-1940', QUICK)
+        result = self.ow.get_stats_heroes_used(PC, AMERICAS, 'elyK-1940', QUICK)
         self.assertEqual(len(result), len(heroes))
