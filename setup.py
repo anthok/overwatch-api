a06f76553385451fb296c7e46af2d7ff83f29463
anthok <kanth1@unh.newhaven.edu>
2017-04-21 21:52:02 -0400

    Version bump and requiring aiohttp version 2.0.0 or greater

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = '0.4'
+__version__ = '0.5'
 
 setup(
       name='overwatch-api',
@@ -12,7 +12,8 @@ setup(
       url='http://github.com/anthok/overwatch-api',
       author='Kyle Anthony <anthok>',
       license='MIT',
-      install_requires=['requests', 'aiohttp', 'async_timeout'],
+      install_requires=['aiohttp>=2.0.0',
+                        'async_timeout'],
       test_suite='nose.collector',
       tests_require=['nose', 'vcrpy', 'asyncio'],
   )
